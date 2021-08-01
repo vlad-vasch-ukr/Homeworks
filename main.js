09d7674d32ef3275b7008b5db653845b49bd3f8a
Vladislav <vasch_ukr@ukr.net>
2021-08-01 19:38:32 +0300

    task 2

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ const capitals = (list) => {
 }
 
 capitals(citiesAndCountries)
+
+//task 2
+
+function getArray(amount) {
+  if (typeof(amount) === 'number' && amount > 0 && amount % 3 === 0) {
+    const subarrayLength = 3
+    const result = []
+    let subarray = []
+    for (let i = 1; i <= amount; i++) {
+      subarray.push(i)
+      if (subarray.length === subarrayLength) {
+        result.push(subarray)
+        subarray = []
+      }
+    }
+    console.log(result)
+    return result
+  } else console.log('Please enter a number greater than zero and a multiple of three!!!')
+}
+
+getArray(12)
